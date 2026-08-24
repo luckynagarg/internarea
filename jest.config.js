db8d1d686f48339b33b64c2be9574f9d54c4c17a
luckynagarg <227975184+luckynagarg@users.noreply.github.com>
2026-08-24 11:19:55 +0530

    23 aug

---
+++ jest.config.js
@@ -0,0 +1,17 @@
+/** @type {import('jest').Config} */
+const nextJest = require('next/jest');
+
+const createJestConfig = nextJest({
+  dir: './',
+});
+
+/** @type {import('jest').Config} */
+const customJestConfig = {
+  testEnvironment: 'node',
+  moduleNameMapper: {
+    '^@/(.*)$': '<rootDir>/src/$1',
+  },
+  testMatch: ['**/__tests__/**/*.test.ts', '**/__tests__/**/*.test.tsx'],
+};
+
+module.exports = createJestConfig(customJestConfig);
