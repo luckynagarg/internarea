ca3b65a80ff1e5e6f0802464bc932b1d2c0e9cae
luckynagarg <227975184+luckynagarg@users.noreply.github.com>
2026-08-24 20:25:26 +0530

    do it

--- src/pages/internship/index.tsx
+++ src/pages/internship/index.tsx
@@ -491,7 +491,7 @@ const Index = () => {
                   className="flex-1 rounded-lg border border-gray-300 px-4 py-2 text-sm font-medium text-gray-700 hover:bg-gray-50"
                 >
                   {t("internship.clearAll")}
-                </button>
+                </button> 
 
                 <button
                   type="button"
